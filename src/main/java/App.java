bef65068c5a16d3807684d5288b4b4f12929f7be
Jamila <meftakhedinova@mail.ru>
2017-12-21 10:53:09 +0400

    Initial Stage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,23 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class App extends Application {
+private Stage primaryStage;
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        initRoot();
+    }
+
+    private void initRoot() throws IOException{
+        AnchorPane pane = (AnchorPane) FXMLLoader.load(getClass().getResource("views/HomePage.fxml"));
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
