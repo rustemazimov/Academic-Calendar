ea8e36ea294c344f3dac48641a5a05311a2efab8
user <user@google.com>
2017-12-26 11:32:41 -0800

    Initial Home Page

--- src/main/java/controllers/HomePageController.java
+++ src/main/java/controllers/HomePageController.java
@@ -1,5 +1,14 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
 public class HomePageController {
+
+    @FXML
+    private TextField searchField;
+
+
 
 }
