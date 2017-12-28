2deca2f7a014f6261ea48413f92651acac85d3e6
Rustem <rustemazimov1999@gmail.com>
2017-12-28 23:33:05 +0400

    fx:id(s)'re set in HomePage.fxml & implemented in HomePageController

--- src/main/java/controllers/HomePageController.java
+++ src/main/java/controllers/HomePageController.java
@@ -1,4 +1,35 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
 public class HomePageController {
+
+    @FXML private ChoiceBox<String> monthChoiceBox;
+
+    @FXML private ChoiceBox<String> yearChoiceBox;
+
+    @FXML private ListView<String> eventListVIew;
+
+    @FXML private TextField searchTextFIeld;
+
+    @FXML private Text statusText;
+
+    @FXML private TextField titleTextField;
+
+    @FXML private TextField detailsTextField;
+
+    @FXML private DatePicker datePicker;
+
+    @FXML private GridPane calendarGridPane;
+
+
+
+
+
 }
